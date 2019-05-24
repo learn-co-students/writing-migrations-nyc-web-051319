9b0c9387f5fee785ec50d931408a30c7be06b222
Will Bainton <baintonw@gmail.com>
2019-05-24 10:09:22 -0400

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,20 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+
+def change
+    create_table :students do |t|
+      t.string :name
+    end
+  end
 end
+
+
+# sql = <<-SQL
+# CREATE TABLE IF NOT EXISTS students (
+#   id INTEGER PRIMARY KEY,
+#   name TEXT
+# )
+# SQL
+
+# create_table :products do |t|
+#   t.string :name
+# end
